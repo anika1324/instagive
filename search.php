587be0646d4f9fbaf06f84ba7817b6e7103a2410
anika1324 <61261022+anika1324@users.noreply.github.com>
2020-03-08 18:53:49 -0700

    Add files via upload

--- search.php
+++ search.php
@@ -1,66 +1,23 @@
-<html>
-<head>
-<title>SEARCH</title>
-</head>
-<body>
-  <input type = "text" name = "Search">
-  <!doctype html>
-  <html>
-  <head>
-  <title>Welcome</title>
-  <style>
-  .header {
-  overflow: hidden;
-  background-color: #f1f1f1;
-  padding: 20px 10px;
+<?php
+if(isset($_POST["submit"])){
+	if(!empty($_POST["search_text"])) {
+    $search_text = $_POST["search_text"];
+    $con=mysqli_connect('localhost','root','') or die(mysqli_error());
+		mysqli_select_db($con,'instagive') or die('Cannot select db');
+		$result= mysqli_query($con,"SELECT * FROM user_details WHERE username LIKE '%".$search_text."%' OR fullname LIKE '%".$search_text."%'");
+    $numrows = mysqli_num_rows($result);
+  while($row=mysqli_fetch_array($result)){
+          $username=$row['username'];
+          $fullname=$row['fullname'];
+?>
+<a href=""><?php echo $fullname;?></a>
+<?php
+          echo "\n";
   }
-
-
-  .header a {
-  float: left;
-  color: black;
-  text-align: center;
-  padding: 9px;
-  text-decoration: none;
-  font-size: 18px;
-  line-height: 25px;
-  border-radius: 4px;
+}
+  else {
+    echo "Empty search string";
   }
+}
 
-
-  .header a.logo {
-  font-size: 25px;
-  font-weight: bold;
-  }
-
-
-  .header a:hover {
-  background-color: #ddd;
-  color: black;
-  }
-
-
-  .header a.active {
-  background-color: dodgerblue;
-  color: white;
-  }
-
-
-  .header-right {
-  float: right;
-  }
-  </style>
-  
-
-  <div class="header">
- <a href="feed.php" class="logo"><img src= "https://qph.fs.quoracdn.net/main-qimg-06544605342d910b700b9cc055fe860c" width= 70px height= 60px></img>InstaGive</a>
- <div class="header-right">
- <a href= uploads.html> <img src ="uploads.png" width=50px height= 50px></img></a>
- <a class="active" href="followers.php"> <img src = "heart.png" width = 40px height = 40px></img></a>
- <a href="discoverpeople.php"><img src = "compass.png" width= 50px height= 50px></img></a>
- <a href="profile.php"><img src = "profile.png" width= 50px height = 50px></img></a>
- <a href = "search.php"><img src = "search.png" width=50px height= 50px></img></a>
-
- </div>
-
- </body>
+?>
